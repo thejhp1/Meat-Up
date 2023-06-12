24a8dd3723f4c8d558b0a4233cabad9543b0fbbe
JP <junhyung96@gmail.com>
2023-06-12 19:02:36 -0400

    working on events seeder

---
+++ backend/db/seeders/20230612225935-events.js
@@ -0,0 +1,45 @@
+'use strict';
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+const { Event } = require('../models');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    options.validate = true
+    await Event.bulkCreate([
+      {
+        groupId: 1,
+        venueId: '',
+        name: '',
+        type: '',
+
+      },
+      {
+        groupId: 2,
+      },
+      {
+        groupId: 3,
+      },
+      {
+
+        groupId: 4,
+      },
+      {
+        groupId: 5,
+      },
+    ], options).catch((err) => {
+      throw new Error(err)
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Events'
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      status: { [Op.in]: ['co-host', 'member', 'pending'] }
+    }, {});
+  }
+};
