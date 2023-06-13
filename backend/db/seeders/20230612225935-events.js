c8f454c6943fceca341733d0006a8ded50e9932d
JP <junhyung96@gmail.com>
2023-06-13 13:07:24 -0400

    test for live

--- backend/db/seeders/20230612225935-events.js
+++ backend/db/seeders/20230612225935-events.js
@@ -8,31 +8,31 @@ const { Event } = require('../models');
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    options.validate = true
-    await Event.bulkCreate([
-      {
-        groupId: 1,
-        venueId: '',
-        name: '',
-        type: '',
+  //   options.validate = true
+  //   await Event.bulkCreate([
+  //     {
+  //       groupId: 1,
+  //       venueId: '',
+  //       name: '',
+  //       type: '',
 
-      },
-      {
-        groupId: 2,
-      },
-      {
-        groupId: 3,
-      },
-      {
+  //     },
+  //     {
+  //       groupId: 2,
+  //     },
+  //     {
+  //       groupId: 3,
+  //     },
+  //     {
 
-        groupId: 4,
-      },
-      {
-        groupId: 5,
-      },
-    ], options).catch((err) => {
-      throw new Error(err)
-    })
+  //       groupId: 4,
+  //     },
+  //     {
+  //       groupId: 5,
+  //     },
+  //   ], options).catch((err) => {
+  //     throw new Error(err)
+  //   })
   },
 
   async down (queryInterface, Sequelize) {
--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,7 +1,37 @@
 const express = require("express");
-const router = express.Router();
 
 const { Group, Membership, GroupImage, Venue, User } = require('../../db/models')
+
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
+const router = express.Router();
+
+const validateGroupSignup = [
+    check('name')
+      .exists({checkFalsy: true})
+      .isLength({ max: 60 })
+      .withMessage("Name must be 60 characters or less"),
+    check('about')
+      .exists({checkFalsy: true})
+      .isLength({ min: 50 })
+      .withMessage("About must be 50 characters or more"),
+    check("type")
+      .exists({ checkFalsy: true })
+      .isIn(['In person','Online'])
+      .withMessage("Type must be 'Online' or 'In person'"),
+    check("private")
+      .exists({ checkFalsy: true })
+      .isBoolean()
+      .withMessage("Private must be a boolean"),
+    check("city")
+      .exists({ checkFalsy: true })
+      .withMessage("City is required"),
+    check("state")
+      .exists({ checkFalsy: true })
+      .withMessage("State is required"),
+      handleValidationErrors,
+];
 
 router.get('/', async (req, res, next) => {
     const groups = await Group.findAll({
@@ -85,6 +115,9 @@ router.get('/:groupId', async (req, res, next) => {
     res.json(list[0])
 })
 
-// router.
+router.post('/', validateGroupSignup, async (req, res, next) => {
+    const { name, about, type, private, city, state } = req.body
+
+})
 
 module.exports = router;
