3367836827b60f36f6a177a779a365f522492248
JP <junhyung96@gmail.com>
2023-06-14 12:31:04 -0400

    PUT /api/venues/:venueId

--- backend/routes/api/venues.js
+++ backend/routes/api/venues.js
@@ -1,7 +1,9 @@
 const express = require("express");
 
 const {
-  Venue
+  Venue,
+  Group,
+  Membership
 } = require("../../db/models");
 
 const { check } = require("express-validator");
@@ -27,35 +29,60 @@ const validateVenueSignup = [
 ];
 
 router.put("/:venueId", validateVenueSignup, async (req, res, next) => {
-  const { address, city, state, lat, lng } = req.body;
-  const venue = await Venue.findByPk(req.params.venueId, {});
+  const { user } = req;
+  if (user) {
+    const group = await Group.findByPk(req.params.groupId);
+    if (!group) {
+      res.status(404);
+      res.json({
+        message: "Group couldn't be found",
+      });
+    }
+    const userCheck = await Membership.findByPk(user.id)
+    if (group.toJSON().organizerId === user.id || (userCheck.toJSON().status == 'co-host' && userCheck.toJSON().groupId === req.params.groupId)){
+      const { address, city, state, lat, lng } = req.body;
+      const venue = await Venue.findByPk(req.params.venueId, {});
 
-  if (!venue) {
-    res.status(404);
+      if (!venue) {
+        res.status(404);
+        res.json({
+          message: "Venue couldn't be found",
+        });
+      }
+
+      if (address) {
+        venue.address = address
+      }
+      if (city) {
+        venue.city = city
+      }
+      if (state) {
+        venue.state = state
+      }
+      if (lat) {
+        venue.lat = lat
+      }
+      if (lng) {
+        venue.lng = lng
+      }
+
+      await venue.save()
+
+      res.json(venue);
+
+    } else {
+      res.status(403);
+      res.json({
+        message: "Forbidden",
+      });
+    }
+  } else {
+    res.status(401);
     res.json({
-      message: "Venue couldn't be found",
+      message: "Authentication required",
     });
   }
 
-  if (address) {
-    venue.address = address
-  }
-  if (city) {
-    venue.city = city
-  }
-  if (state) {
-    venue.state = state
-  }
-  if (lat) {
-    venue.lat = lat
-  }
-  if (lng) {
-    venue.lng = lng
-  }
-
-  await venue.save()
-
-  res.json(venue);
 });
 
 module.exports = router;
