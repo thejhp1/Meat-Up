0e4dc395e670b88b734e50ac852006f65a485424
JP <junhyung96@gmail.com>
2023-06-30 19:37:53 -0400

    finished landing page (link to all group/event/new group not implemented

--- frontend/src/components/LandingPage/GroupEventLinks.js
+++ frontend/src/components/LandingPage/GroupEventLinks.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import GroupImage from "../../images/handsUp.svg"
-import EventImage from "../../images/ticket.svg"
-import NewGroupImage from "../../images/joinGroup.svg"
+import GroupImage from "../../images/handsUp.svg";
+import EventImage from "../../images/ticket.svg";
+import NewGroupImage from "../../images/joinGroup.svg";
+import { useSelector } from "react-redux";
 
 export const GroupEventLinks = () => {
+  const sessionUser = useSelector((state) => state.session.user);
+
   return (
     <div className="group-event-links">
       <h2>How Meatup works</h2>
@@ -14,21 +17,41 @@ export const GroupEventLinks = () => {
       </p>
       <div className="link-container">
         <div className="group-image">
-            <img src={GroupImage}></img>
-            <Link className="link" to={"/groups"}>See all groups</Link>
-            <p>Do what you love, meet others who love it, find your community. The rest is history!</p>
+          <img src={GroupImage}></img>
+          <Link className="link" to={"/groups"}>
+            See all groups
+          </Link>
+          <p>
+            Do what you love, meet others who love it, find your community. The
+            rest is history!
+          </p>
         </div>
         <div className="event-image">
-            <img src={EventImage}></img>
-            <Link className="link" to={"/events"}>Find an event</Link>
-            <p>Events are happening on just about any topic you can think of, from online gaming and photography to yoga and hiking.</p>
+          <img src={EventImage}></img>
+          <Link className="link" to={"/events"}>
+            Find an event
+          </Link>
+          <p>
+            Events are happening on just about any topic you can think of, from
+            online gaming and photography to yoga and hiking.
+          </p>
         </div>
         <div className="join-image">
-            <img src={NewGroupImage}></img>
-            <Link className="link join-group" to={"/groups"}>
-            Start a group
+          <img src={NewGroupImage}></img>
+          {sessionUser ? (
+            <Link
+              className="link"
+              to={"/groups"}
+            >
+              Start a group
             </Link>
-            <p>You don't have to be an expert to gather people together and explore shared interests.</p>
+          ) : (
+            <span className="join-group">Start a group</span>
+          )}
+          <p>
+            You don't have to be an expert to gather people together and explore
+            shared interests.
+          </p>
         </div>
       </div>
     </div>
